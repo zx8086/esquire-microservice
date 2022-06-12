87204c48188772777c20b47965127afc0df1c985
zx8086 <zx8086@mac.com@users.noreply.github.com>
2022-06-12 18:23:19 +0200

    Add logging files + fix code smells

--- esquire-service.js
+++ esquire-service.js
@@ -1,39 +1,60 @@
 // Require express and create an instance of it
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const axios = require('axios');
 
 const logger = require('./logger')
-// const { configFromPath } = require('./util');
 const httpLogger = require('./httpLogger')
 
 app.use(httpLogger)
 
 // on the request to root (localhost:3002/)
-app.get('/', function (req, res) {
+app.get('/', function (_req, res) {
     logger.debug('This is the "/" route.')
     logger.info("Welcome to the Esquire Micro-service")
-    return res.status(200).send({ message: "Welcome to the Esquire Micro-service" });
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end('Welcome to the Esquire Micro-service')
+
 });
 
-app.get("/esquire", async (req, res) => {
+app.get("/esquire", async (_req, res) => {
     logger.debug('This is the "/esquire" route.')
     logger.info("Simon Owusu Esq ")
     res.status(200).send("Simon Owusu Esq :)");
 });
 
-app.get("/go", async (req, res) => {
+app.get("/go", async (_req, res) => {
     logger.debug('This is the "/go" route.')
     logger.info("Calling Golang Service...")
-    const result = await axios({
+    await axios({
       method: 'GET',
       url: 'http://192.168.0.9:4000/go'
     })
-    return res.status(200).send({ message: "Calling Golang Service..." });
+    .then(function (response) {
+      logger.info('Calling Golang Service...')
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json')
+      res.end('Calling Golang Service...')
+      console.log(response);
+    })
+    .catch(function (error) {
+      logger.error('Failed to call Golang Service...')
+      logger.error('Application Error - ', error)
+      res.statusCode = 500
+      res.setHeader('Content-Type', 'application/json')
+      res.end('Failed to call Golang Service...')
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+      logger.debug('This is the "/esquire" route.')
+    }); 
 });
 
 // Change the 404 message modifing the middleware
-app.use(function(req, res, next) {
+app.use(function(_req, res) {
     logger.debug('This is for erroneous route.')
     logger.info("Sorry, that route doesn't exist. Have a nice day :)")
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
