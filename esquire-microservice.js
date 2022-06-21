b2f46fb27639458a8da5908a8e4879b2a621f332
zx8086 <zx8086@mac.com@users.noreply.github.com>
2022-06-21 11:24:08 +0200

    Minor fix

--- esquire-microservice.js
+++ esquire-microservice.js
@@ -2,28 +2,6 @@
 // const instrument = require('@aspecto/opentelemetry');
 // instrument({aspectoAuth: '1cbb856b-0558-4e75-876f-3aee212f65c7'});
 
-const uptrace = require('@uptrace/node')
-
-uptrace.configureOpentelemetry({
-  // Set dsn or UPTRACE_DSN env var.
-  dsn: 'https://d4FeQAgBhnrERj69gZLYZQ@uptrace.dev/681',
-
-  serviceName: 'myservice',
-  serviceVersion: '1.0.0',
-
-  instrumentations: [
-    {
-      plugins: {
-        express: {
-          enabled: true,
-          path: '@opentelemetry/plugin-express',
-        },
-      },
-    },
-  ],
-})
-
-
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -36,8 +14,7 @@ const httpLogger = require('./httpLogger')
 
 const kafkaInst = require("./kafka");
 
-const consume = require("./consume");
-
+// const consume = require("./consume");
 
 const consumer = kafkaInst.consumer({ groupId: process.env.GROUP_ID })
 
@@ -68,7 +45,7 @@ app.get('/', function (_req, res) {
 
 });
 
-app.get("/esquire", async (_req, res) => {
+app.get("/esquire", async (_req, res, next) => {
     logger.debug('This is the "/esquire" route.')
     logger.info("Simon Owusu Esq ")
     res.status(200).send("Simon Owusu Esq :)");
@@ -83,12 +60,12 @@ app.get("/consume", async (_req, res) => {
       logger.debug("Console Error....");
     } catch (e) {
       console.error("Failed to gracefully disconnect consumer", e);
-      await consumer.disconnect();
+      // await consumer.disconnect();
     }
     finally {
       console.log(`All Tasks are Done`);
       res.end("Consumed all Kafka messages...");
-      await consumer.disconnect();
+      // await consumer.disconnect();
       process.exit(1);
     }
   });
--- tracing-esquire.js
+++ tracing-esquire.js
@@ -15,6 +15,7 @@ const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumenta
 const { KafkaJsInstrumentation } = require('opentelemetry-instrumentation-kafkajs');
 const { RouterInstrumentation } = require('@opentelemetry/instrumentation-router');
 const { SocketIoInstrumentation } = require('opentelemetry-instrumentation-socket.io');
+const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 
 const sdk = new opentelemetry.NodeSDK({
@@ -22,6 +23,14 @@ const sdk = new opentelemetry.NodeSDK({
   instrumentations: 
     [
       getNodeAutoInstrumentations(),
+      new ExpressInstrumentation({
+        requestHook: (span, requestInfo) => {
+          span.setAttribute(
+            "http.request.body",
+            JSON.stringify(requestInfo.req.body)
+          );
+        },
+      }),
       // new RouterInstrumentation(),
       // new SocketIoInstrumentation(),
       new KafkaJsInstrumentation() 
@@ -38,8 +47,18 @@ const provider = new NodeTracerProvider({
 const exporter = new OTLPTraceExporter();
 
 // Configure span processor to send spans to the exporter
-provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+// provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 provider.register();
 
 sdk.start()
+  .then(() => console.log('Tracing initialized'))
+  .catch((error) => console.log('Error initializing tracing', error));
+
+  // gracefully shut down the SDK on process exit
+  process.on('SIGTERM', () => {
+  sdk.shutdown()
+  .then(() => console.log('Tracing terminated'))
+  .catch((error) => console.log('Error terminating tracing', error))
+  .finally(() => process.exit(0));
+  });
