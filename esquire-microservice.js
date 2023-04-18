2abcc7f1351ace3c6c7b3d70357b0678db2905bd
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2023-04-18 11:43:33 +0200

    New Telemetry Exporter

--- esquire-microservice.js
+++ esquire-microservice.js
@@ -1,17 +1,17 @@
-// Require express and create an instance of it
-// const instrument = require('@aspecto/opentelemetry');
+const http = require('http')
+const port = process.env.PORT || 8072
 
-const dotenv = require("dotenv");
-dotenv.config();
+const dotenv = require("dotenv")
+dotenv.config()
 
-const express = require('express');
-const app = express();
-app.disable("x-powered-by");
-const axios = require('axios');
+const express = require('express')
+const app = express()
+app.disable("x-powered-by")
+const axios = require('axios')
 const logger = require('./logger')
 const httpLogger = require('./httpLogger')
 
-const kafkaInst = require("./kafka");
+const kafkaInst = require("./kafka")
 
 // const consume = require("./consume");
 
@@ -24,20 +24,20 @@ app.get('/', function (_req, res) {
     res.setHeader('Content-Type', 'application/json')
     res.end('Welcome to the Esquire Micro-service')
 
-});
+})
 
 app.get("/trace", async (_req, res, next) => {
     logger.debug('This is the "/esquire" route.')
     logger.info("Simon Owusu Esq ")
-    res.status(200).send("Simon Owusu Esq :)");
-});
+    res.status(200).send("Simon Owusu Esq :)")
+})
 
 app.use(function(_req, res) {
     logger.debug('This is for erroneous route.')
     logger.info("Sorry, that route doesn't exist. Have a nice day :)")
-    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
-});
+    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)")
+})
 
-app.listen(8072, function () {
-    console.log('Esquire Service is listening on port 8072.');
-});
+app.listen({port}, function () {
+    console.log('Esquire Service is listening on port 8072.')
+})
