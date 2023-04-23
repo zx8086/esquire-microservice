5531da75df879b9a17d9106c15698622b14fd6d2
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2023-04-23 21:53:59 +0200

    Change content type

--- esquire-microservice.js
+++ esquire-microservice.js
@@ -13,29 +13,25 @@ const httpLogger = require('./httpLogger')
 
 const kafkaInst = require("./kafka")
 
-// const consume = require("./consume");
-
 app.use(httpLogger)
 
 app.get('/', function (_req, res) {
     logger.debug('This is the "/" route.')
     logger.info("Welcome to the Esquire Micro-service")
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end('Welcome to the Esquire Micro-service')
+    res.status(200).setHeader('Content-Type', 'application/json').send('Welcome to the Esquire Micro-service')
 
 })
 
-app.get("/trace", async (_req, res, next) => {
+app.get("/trace", async (_req, res) => {
     logger.debug('This is the "/esquire" route.')
     logger.info("Simon Owusu Esq ")
-    res.status(200).send("Simon Owusu Esq :)")
+    res.status(200).setHeader('Content-Type', 'application/json').send("Simon Owusu Esq :)")
 })
 
 app.use(function(_req, res) {
     logger.debug('This is for erroneous route.')
     logger.info("Sorry, that route doesn't exist. Have a nice day :)")
-    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)")
+    res.status(404).setHeader('Content-Type', 'application/json').send("Sorry, that route doesn't exist. Have a nice day :)")
 })
 
 app.listen({port}, function () {
